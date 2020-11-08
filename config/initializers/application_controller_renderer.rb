be53c630e029f53acee69b463a9ba2b5298334d1
taketoyamori <ymymz87@icloud.com>
2020-11-08 19:41:43 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '8e98fb0bb0a836edd149a337139b01e163332bab046192cd63f118158f8cf14df4061d154b5e408917bc302ace036acf2433b125fe287ac0e96f4c05de04644e'
